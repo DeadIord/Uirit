4e8fe423c247dcaebaef30a1c6c3a3b30246b7f5
Илья <plehanov.iluha@gmail.com>
2025-03-09 22:00:58 +0400

    - Просмотр записей Система 2 - Функция измениния статтуса при нажатии на кнопку

--- WebSystemTwo/Controllers/HomeController.cs
+++ WebSystemTwo/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-using MassTransit;
 using Microsoft.AspNetCore.Mvc;
-using SendService.Core.Commands;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using WebSystemTwo.Data;
 using WebSystemTwo.Models;
 
 namespace WebSystemTwo.Controllers
@@ -9,24 +8,37 @@ namespace WebSystemTwo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public async Task<IActionResult> IndexAsync()
         {
-
-            return View();
+            var applications = await _db.Applications.Include(a => a.Status).ToListAsync();
+            return View(applications);
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Register(long id)
         {
-            return View();
-        }
+            var application = await _db.Applications.Include(a => a.Status)
+                                                    .FirstOrDefaultAsync(a => a.Id == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
 
-       
+            application.StatusId = 4;
+            application.ServiceNumber = string.Format("Присвоен регистрационный № {0} от {1}",
+                                                      application.Id,
+                                                      DateTime.Now.ToString("dd.MM.yyyy"));
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(IndexAsync));
+        }
     }
 }
---
+++ WebSystemTwo/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WebSystemTwo.Models;
+
+namespace WebSystemTwo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationModel> Applications { get; set; }
+
+        public DbSet<StatusModel> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationModel>()
+                .HasOne(a => a.Status)
+                .WithMany()
+                .HasForeignKey(a => a.StatusId);
+
+            modelBuilder.Entity<StatusModel>().HasData(
+                new StatusModel
+                {
+                    Id = 4,
+                    StatusCode = 1050,
+                    StatusName = "Запрос зарегистрирован",
+                    Text = "Присвоен регистрационный № {0} от {1}. Запрос принят к рассмотрению."
+                }
+            );
+        }
+    }
+}
